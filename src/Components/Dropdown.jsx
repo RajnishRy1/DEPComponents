f5648e5dbe287f87debb7c43a2e78e2adc6852c2
Rajnish Virpate <rajnish.virpate@ilink-systems.com>
2025-01-30 15:27:08 +0530

    State management logic

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -2,19 +2,37 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Dropdown = ({ countries }) => {    
+    useEffect(() => {
+        if (window.CustomElement) {
+          window.CustomElement.init((element) => {
+            // console.log("Custom Element loaded", element);
+            if (element.value) {
+                setSelectedCountry(element.value);
+              }
+          });
+        }
+      }, []);
     const [selectedCountry, setSelectedCountry] = useState('Select a country');
-
+    const handleChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedCountry(selectedValue);
+    
+        // Notify Kontent.ai about the change
+        CustomElement.setValue(selectedValue);
+      };
     return (
         <div className="container mt-6">
-            <select className="form-select" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)}>
+            <select className="form-select" value={selectedCountry} onChange={handleChange}>
                 <option>Select a country</option>
                 {countries.map((country, index) => <option key={index}>{country}</option>)}
             </select>
         </div>
     );
 };  
+
+
 
 export default Dropdown;
