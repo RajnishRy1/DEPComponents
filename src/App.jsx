1262c863275d23e1631e0c7160236ece6d66c276
Rajnish Virpate <rajnish.virpate@ilink-systems.com>
2025-02-10 18:37:36 +0530

    Added IFrame as a custom element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
 // import './App.css'
-import Dropdown from './Components/Dropdown'
-import { Countries } from './utils/Countries'
 
 function App() {
 
   return (
     <>
-      <Dropdown countries={Countries} />
+
+      <div className="App">
+        <header className="App-header">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.jsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+        </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Dropdown from './Components/Dropdown.jsx'
+import { Countries } from './utils/Countries.js'
+
 // import './index.css'
 import App from './App.jsx'
+import IFrame from './Components/IFrame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App />
+       */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dropdown" element={<Dropdown countries={Countries}/>} />
+        <Route path="/iframe" element={<IFrame />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
